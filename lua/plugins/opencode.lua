af6b551e5e53015f8a6bc1d8f7d79ee45adf6ef8
Riley Mathews <rileymathews80@gmail.com>
2026-03-31 23:22:34 -0500

    fix hotkey

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -58,7 +58,7 @@ return {
     vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
     vim.keymap.set({ "n" }, "<leader>ot", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
 
-    vim.keymap.set({ "n", "x" }, "<leader>oa",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
+    vim.keymap.set({ "n", "x" }, "<leader>ol",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
     vim.keymap.set("n",          "<leader>ov", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
 
     vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
