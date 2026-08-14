c9e5a7dd2e7de11030b33cf77a87602c6a866971
Riley Mathews <rileymathews80@gmail.com>
2026-08-14 14:38:00 -0500

    tmux and herdr

--- lua/plugins/herdr.lua
+++ lua/plugins/herdr.lua
@@ -1,7 +1,15 @@
 return {
-  dir = vim.fn.expand("~/.local/share/vim-herdr-navigation"),
+  "christoomey/vim-tmux-navigator",
   lazy = false,
+
+  init = function()
+    -- Keep the :TmuxNavigate* commands, but don't let this plugin
+    -- install <C-h/j/k/l> mappings.
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
+
   config = function()
+    -- This becomes the single owner of <C-h/j/k/l>.
     dofile(
       vim.fn.expand(
         "~/.local/share/vim-herdr-navigation/editor/nvim.lua"
